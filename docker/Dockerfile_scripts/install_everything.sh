9a0d8c4dad3efa5620deac7486816c6b8249061e
Felix-1871 <fellixmanning@gmail.com>
2026-04-10 19:02:44 +0200

    changed the dockerfile so it will install mandarin plugin

--- docker/Dockerfile_scripts/install_everything.sh
+++ docker/Dockerfile_scripts/install_everything.sh
@@ -4,11 +4,11 @@
 # in the Dockerfile.
 
 # Mecab
-apt-get update -y
-apt-get install -y mecab mecab-ipadic-utf8
-apt-get clean && rm -rf /var/lib/apt/lists/*
+# apt-get update -y
+# apt-get install -y mecab mecab-ipadic-utf8
+# apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # Plugins
 pip install lute3-mandarin
-pip install lute3-thai
-pip install lute3-khmer
+# pip install lute3-thai
+# pip install lute3-khmer
